41e524677af1644d82a19381b4650afb88ea2239
Aravinth vadivel <aravinthgithub7@gmail.com>
2025-08-20 11:17:25 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ set -e
 IMAGE_NAME="aravinthdevops/prod-repo"
 TAG="latest"
 
-echo "Pulling latest image..."
+echo "🛠️ Pulling latest image..."
 docker pull $IMAGE_NAME:$TAG
 
-echo "Deploying with Docker Compose..."
-docker-compose up -d
+echo "🚀 Deploying with Docker Compose..."
+docker compose -f docker-compose.yml up -d
